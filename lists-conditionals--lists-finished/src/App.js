7e6f58de67d4c8faa8e8945adf7366994f748174
Grzegorz Falba <grzegorz.falba@accenture.com>
2020-04-03 15:43:55 +0200

    Poprawki

--- lists-conditionals--lists-finished/src/App.js
+++ lists-conditionals--lists-finished/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
 import CarTable from './CarListHandler/CarListHandler'
-import AddCar from '../src/AddCar/AddCar';
-
 
 function App() {
   return <div>
-  <CarTable/>
-  <br></br>
-  <br></br>
-  <AddCar/>
+  <CarTable />
   </div>
 } 
     
--- lists-conditionals--lists-finished/src/CarListHandler/CarListHandler.js
+++ lists-conditionals--lists-finished/src/CarListHandler/CarListHandler.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 
 import classes from '../CarListHandler/CarListHandler.css'
+import AddCar from "../AddCar/AddCar";
 
 class carTable extends Component {
-    state = {
-      cars: [
-        { id: 1, marka: 'Audi', nazwa: 'A4',  dataProd: 2002, cena: 20000, akcja: "Usuń"},
-        { id: 2, marka: 'Toyota', nazwa: 'Corolla',  dataProd: 1999, cena: 5000, akcja: "Usuń" },
-        { id: 3, marka: 'Honda', nazwa: 'Civic',  dataProd: 2010, cena: 25000, akcja: "Usuń" },
-      ]
+    constructor(props) {
+        super(props);
+        this.rerenderCarTableCallback = this.rerenderCarTableCallback.bind(this);
     }
 
+    state = {
+        cars: [
+            {id: 1, marka: 'Audi', nazwa: 'A4', dataProd: 2002, cena: 20000, akcja: "Usuń"},
+            {id: 2, marka: 'Toyota', nazwa: 'Corolla', dataProd: 1999, cena: 5000, akcja: "Usuń"},
+            {id: 3, marka: 'Honda', nazwa: 'Civic', dataProd: 2010, cena: 25000, akcja: "Usuń"},
+        ]
+    };
+
     detailLook = () => {
-      const cars = []
-      let i = 0
+      const cars = [];
+      let i = 0;
       if (this.state.cars) {
         for (const car of this.state.cars){
-          i++
+          i++;
           cars.push(<tr key={car.id}onClick={() => this.renderCarTable(car.id)}>
             <td>{i}</td>
             <td>{car.marka}</td>
@@ -27,7 +33,7 @@ class carTable extends Component {
         }
       }
       return cars;
-    } 
+    };
     deleteChangedHandler = ( event, id ) => {
         const carIndex = this.state.cars.findIndex(p => {
           return p.id === id;
@@ -43,49 +49,65 @@ class carTable extends Component {
         cars[carIndex] = car;
     
         this.setState( {cars: cars} );
-      }
+    };
 
-      deleteCarHandler = (carIndex) => {
+    deleteCarHandler = (carIndex) => {
         const cars = [...this.state.cars];
         cars.splice(carIndex, 1);
         this.setState({cars: cars});
-      }
-renderCarTable () {
-    return this.state.cars.map((car, index) => {
-        const {id, marka, nazwa, dataProd, cena, akcja} = car
-        return (
-            <tr key = {id}>
-                <td>{id}</td>
-                <td>{marka}</td>
-                <td>{nazwa}</td>
-                <td>{dataProd}</td>
-                <td>{cena}</td>
-                <td><button onClick={() => this.deleteCarHandler(index)}>{akcja}</button></td>
-            </tr>
+    };
 
+    renderCarTable () {
+        return this.state.cars.map((car, index) => {
+            const {id, marka, nazwa, dataProd, cena, akcja} = car;
+            return (
+                <tr key = {id}>
+                    <td>{id}</td>
+                    <td>{marka}</td>
+                    <td>{nazwa}</td>
+                    <td>{dataProd}</td>
+                    <td>{cena}</td>
+                    <td><button onClick={() => this.deleteCarHandler(index)}>{akcja}</button></td>
+                </tr>
+
+            )
+        }
         )
     }
-    )
-}
-renderCarTableHeader () {
-    let header = Object.keys(this.state.cars[0])
-    return header.map((key, index) => {
-        return <th key = {index}>{key.toUpperCase()}</th>
-    })
-}
-render () { 
-    return (
-        <div className="App">
-        <h1 id='title' className={classes.title}>Wypożyczalnia aut</h1>
-        <p>Dostępne auta</p>
-        <table id = 'cars' className={classes.cars}>
-            <tbody>
-                <tr>{this.renderCarTableHeader()}</tr>
-                {this.renderCarTable()}
-                {this.detailLook()}
-            </tbody>
-        </table>
-      </div>
+
+    renderCarTableHeader () {
+        let header = Object.keys(this.state.cars[0]);
+        return header.map((key, index) => {
+            return <th key = {index}>{key.toUpperCase()}</th>
+        })
+    }
+
+    rerenderCarTableCallback(cars) {
+        this.setState({
+            cars: cars
+        });
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <h1 id='title' className={classes.title}>Wypożyczalnia aut</h1>
+                <p>Dostępne auta</p>
+                <table id = 'cars' className={classes.cars}>
+                    <tbody>
+                        <tr>{this.renderCarTableHeader()}</tr>
+                        {this.renderCarTable()}
+                        {this.detailLook()}
+                    </tbody>
+                </table>
+
+                <br />
+                <br />
+                <AddCar
+                    cars={this.state.cars}
+                    onChange={this.rerenderCarTableCallback}
+                />
+            </div>
         );
     };
 }
